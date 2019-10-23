ee2f1a7171e15c06b0700a806c1be98dac98f460
刘斌帅 <leo_1995@126.com>
2019-10-23 10:58:44 +0800

    chain of responsibility

--- chain-of-responsibility/src/indi/leo/design/chain/App.java
+++ chain-of-responsibility/src/indi/leo/design/chain/App.java
@@ -3,11 +3,24 @@ package indi.leo.design.chain;
 public class App {
 
     public static void main(String[] args) {
+        //before refactoring
+        AHandler aHandler = new AHandler();
+        BHandler bHandler = new BHandler();
+        CHandler cHandler = new CHandler();
+        Request request = new Request();
+        if (aHandler.canHandle(request)) {
+            System.out.println(aHandler.doHandle(request));
+        }else if (bHandler.canHandle(request)) {
+            System.out.println(bHandler.doHandle(request));
+        }else if (cHandler.canHandle(request)) {
+            System.out.println(cHandler.doHandle(request));
+        }
+        //after refactoring
         AbstractHandler handler = new AHandler()
                 .withNextHandler(
                         new BHandler()
                                 .withNextHandler(new CHandler())
                 );
-        System.out.println(handler.handleRequest(new Request()));
+        System.out.println(handler.handleRequest(request));
     }
 }
